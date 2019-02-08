af64bb5269a281d842e1663e765e609dbefd11c2
Scratcho <39732248+Scratcho@users.noreply.github.com>
2019-02-07 21:56:16 -0300

    Primer commit
    Clase 1, ingreso. resolución ejercicios 1 al 4

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,12 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+
+//This function ask to the user for his name and show it with an alert
 function mostrar()
 {
-	
+	var name;
+	name = prompt("Please enter your name", "Jonh Smith");
+	alert(name);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,9 @@
 */
 function mostrar()
 {
-
+	var nombre;
+	nombre = prompt("Ingrese su nombre");
+	//elNombre.value = nombre;
+	document.getElementById('elNombre').value = nombre;
 }
 
