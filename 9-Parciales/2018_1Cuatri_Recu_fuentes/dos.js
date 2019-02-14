fe4f74b1049d6dec8946b41d766bfe4413bf78d3
murraymatias <murray.matias@gmail.com>
2019-02-14 19:54:25 -0300

    Ejercicio dos completado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
-  
+	var precio;
+	var descuento;
+	var roboPorAfip;
+
+	precio = document.getElementById('elPrecio').value;
+
+	precio = parseInt(precio);
+
+	descuento = precio * 0.9;
+
+	roboPorAfip = descuento * 1.21;
+
+	alert("Tu compra es de $"+precio+" tenes un descuento del 10% queda en $"+descuento+", y con el robo de AFIP $"+roboPorAfip);
 }
