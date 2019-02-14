97489b1a51f15f0feb80da8cacf6cc5ad4b0a225
murraymatias <murray.matias@gmail.com>
2019-02-14 20:24:22 -0300

    Ejercicio 4 completado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
+	var precio;
+	var cantidadDeAmigos;
+	var precioConPropina;
+	var precioPorPersona;
+	var precioConImpuesto;
 
+	precio = prompt("Ingrese el precio de la comida: ");
+	cantidadDeAmigos = prompt("Entre cuantas personas desea dividir la cuenta: ");
+
+	precio = parseInt(precio);
+	cantidadDeAmigos = parseInt(cantidadDeAmigos);
+
+	precioConPropina = precio * 1.1;
+
+	precioConImpuesto = precioConPropina * 1.21;
+
+	precioPorPersona = precioConImpuesto / cantidadDeAmigos;
+
+	alert("El precio del servicio es $"+precio+" agregando la propina es $"+precioConPropina+" sumando el IVA que te roba AFIP sería $"+precioConImpuesto+" y como ustedes son "+cantidadDeAmigos+", cada uno debe pagar $"+precioPorPersona);
 }
