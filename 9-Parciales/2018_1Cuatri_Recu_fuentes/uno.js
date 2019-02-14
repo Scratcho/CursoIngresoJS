eb95c46ae97c80dae2374c8d20732e57d786e534
murraymatias <murray.matias@gmail.com>
2019-02-14 19:22:43 -0300

    Primer ejercicio finalizado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+var primerMascota;
+var segundaMascota;
+var pesoPrimerMascota;
+var pesoSegundaMascota;
+var pesoTotal;
 
+primerMascota = prompt("Ingrese el nombre de su primer mascota");
+pesoPrimerMascota = prompt("Ingrese el peso de "+primerMascota);
+segundaMascota = prompt("Ingrese el nombre de su segunda mascota");
+pesoSegundaMascota = prompt("Ingrese el peso de "+segundaMascota);
+
+pesoPrimerMascota = parseInt(pesoPrimerMascota);
+pesoSegundaMascota = parseInt(pesoSegundaMascota);
+
+pesoTotal = pesoPrimerMascota + pesoSegundaMascota;
+
+alert("Tenes dos mascotas "+primerMascota+" y "+segundaMascota+", que pesan "+pesoPrimerMascota+" y "+pesoSegundaMascota+" kilos, la suma de los kilos es "+pesoTotal);
 }
